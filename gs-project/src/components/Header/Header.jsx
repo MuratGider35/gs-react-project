cbcd16bc371ac6688ce6c15e86b31df8795aa374
MuratGider35 <m.gider35@gmail.com>
2022-10-12 16:53:57 +0300

    created gs pages

---
+++ gs-project/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <a href="#">Anasayfa</a>
+      <a href="#">Bülten</a>
+      <a href="#">İletişim</a>
+      <a href="#">Kadro</a>
+    </div>
+  );
+};
+
+export default Header;
---
+++ gs-project/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Main.scss";
+
+const Main = ({data}) => {
+   
+  return <div className="card">
+    <p>{title}</p>
+
+  </div>;
+};
+
+export default Main;
+const { id, title, desc, image } = props

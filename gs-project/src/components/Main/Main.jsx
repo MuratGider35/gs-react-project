0dd74873728a90a37aa4a918511c3678178b72b5
MuratGider35 <m.gider35@gmail.com>
2022-10-13 14:55:09 +0300

    finished project

--- gs-project/src/components/Main/Main.jsx
+++ gs-project/src/components/Main/Main.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "./Main.scss";
+import data from "../../data";
 
-const Main = ({data}) => {
-   
-  return <div className="card">
-    <p>{title}</p>
-
-  </div>;
+const Main = () => {
+  return (
+    <div className="card-container">
+      {data.map((item) => {
+        const { id, title, desc, image } = item;
+        return (
+          <div key={id} className="cards">
+            <div className="title">
+              <h1>{title}</h1>
+            </div>
+            <img src={image} alt="img" />
+            <div className="card-over">
+              <p>{desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Main;
-const { id, title, desc, image } = props
